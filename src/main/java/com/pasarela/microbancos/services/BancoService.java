356b25b4e9b852685908ae6e58e8759b5385c7a2
Nicolás Arias <josue.arias@epn.edu.ec>
2021-09-13 19:21:04 -0500

    Add banco service

--- src/main/java/com/pasarela/microbancos/services/BancoService.java
+++ src/main/java/com/pasarela/microbancos/services/BancoService.java
@@ -1,5 +1,21 @@
 package com.pasarela.microbancos.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pasarela.microbancos.models.Banco;
+import com.pasarela.microbancos.repositories.BancoRepository;
+
+@Service
 public class BancoService {
-    
+
+    @Autowired
+	private BancoRepository bancoRepository;
+		
+	public List<Banco> getAllPersonas (){
+		return bancoRepository.findAll();
+	}
+
 }
