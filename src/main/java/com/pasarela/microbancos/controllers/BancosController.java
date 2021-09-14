61bf4ba6f2e8700de7b0db8b06ce9d8ada9b33e0
Stalyn-1994 <stalyn.vaz@gmail.com>
2021-09-14 10:11:27 -0500

    ok

--- src/main/java/com/pasarela/microbancos/controllers/BancosController.java
+++ src/main/java/com/pasarela/microbancos/controllers/BancosController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
-@RequestMapping(path="/rest/bancos")
+@RequestMapping(path="/")
 public class BancosController {
     
     @Autowired
